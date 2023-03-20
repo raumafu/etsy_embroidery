44c06601221c927386ace7379ca147d4d644a709
rau <omerlefaruk@hotmail.com>
2023-03-21 00:40:31 +0300

    added move to center by bbox

--- extension_convert.py
+++ extension_convert.py
@@ -1,18 +1,25 @@
 from extension_convert_functions import *
+import pyembroidery
+from pyembroidery import EmbPattern, EmbThread
+
 
 input_folder = r"C:\Users\Rau\Desktop\emb_prep"
 output_folder = r"C:\Users\Rau\Desktop\emb_prep\output_files"
-input_file = r"C:\Users\Rau\Desktop\emb_prep\Máquina de Costura .xxx"
-output_file = r"C:\Users\Rau\Desktop\emb_prep\output_files\Máquina de Costura .pes"
+input_file = r"C:\Users\Rau\Desktop\emb_prep\Máquina de Costura.pes"
+output_file = r"C:\Users\Rau\Desktop\emb_prep\output_files\tested_switched4.pes"
 emb_extension_write = ["dst","exp", "jef", "pes", "vp3", "xxx", "emb"]
 emb_extension_read = ["dst","exp", "hus", "jef", "pes", "vp3", "xxx", "dgt", "sew", "pcs", "pcm", "csd", "pec", "shv", "vip", "art", "emb"]
 
 
-#.get_as_stitchblock() returns a list of stitches
 
 
-#Create output folder if it doesn't exist
-makeDir(output_folder)
+design = pyembroidery.read(input_file)
+
+# Move the design to the origin (0, 0)
+move_design_to_origin(design)
+
+# Save the modified design
+pyembroidery.write(design, output_file)
 
 
-pyembroidery.convert(input_file, output_file)
+
--- extension_convert_functions.py
+++ extension_convert_functions.py
@@ -6,3 +6,16 @@ def makeDir(output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     return output_folder
+
+
+def move_design_to_origin(design):
+    min_x, min_y = float('inf'), float('inf')
+
+    for stitch in design.stitches:
+        x, y, cmd = stitch
+        min_x = min(min_x, x)
+        min_y = min(min_y, y)
+
+    for i, stitch in enumerate(design.stitches):
+        x, y, cmd = stitch
+        design.stitches[i] = (x - min_x, y - min_y, cmd)
