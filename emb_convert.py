66c2264cde1f631490af3070da142eb6915b5d57
rau <omerlefaruk@hotmail.com>
2023-03-22 22:54:22 +0300

    scale and save function added

--- emb_convert.py
+++ emb_convert.py
@@ -1,17 +1,19 @@
 from convert_functions import *
 
 input_folder = r"C:\Users\Rau\Desktop\emb_prep"
-output_folder = r"C:\Users\Rau\Desktop\emb_prep\output_files"
-input_file = r"C:\Users\Rau\Desktop\emb_prep\Máquina de Costura .xxx"
-output_file = r"C:\Users\Rau\Desktop\emb_prep\output_files\Máquina de Costura .pes"
+output_folder = r"C:\Users\Rau\Desktop\emb_prep"
+input_file = r"C:\Users\Rau\Desktop\emb_prep\input.pes"
+output_file = r"C:\Users\Rau\Desktop\emb_prep\scaled_out.pes"
 emb_extension_write = ["dst","exp", "jef", "pes", "vp3", "xxx", "emb"]
 emb_extension_read = ["dst","exp", "hus", "jef", "pes", "vp3", "xxx", "dgt", "sew", "pcs", "pcm", "csd", "pec", "shv", "vip", "art", "emb"]
-
-
-
-
-input_file = "input.pes"
-output_file = "centered_output.pes"
 hoop_center = (240, 240)
+scale_factors = [0.7, 0.8, 0.9, 1,2]
+pattern = pyembroidery.read(input_file)
 
-center_embroidery_file(input_file, output_file, hoop_center)
+
+
+
+
+
+
+
