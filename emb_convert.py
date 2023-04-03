cc1bde7305b4199268457027b32df42de86007a2
raumafu <72530298+raumafu@users.noreply.github.com>
2023-04-03 08:08:18 +0300

    refactoring

--- emb_convert.py
+++ emb_convert.py
@@ -4,7 +4,7 @@ from convert_functions import *  # Import all functions from the convert_functio
 input_file = r"C:\Users\Rau\Desktop\emb_prep\input.pes"  # Input embroidery file path
 output_folder = r"C:\Users\Rau\Desktop\emb_prep\scaled"  # Output folder for the scaled embroidery files
 hoop_center = (350, 350)  # Center coordinates of the embroidery hoop
-# pattern = pyembroidery.read(input_file)
+
 # Define scale factors to apply on the embroidery design
 scale_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
 
@@ -35,3 +35,4 @@ end_time = time.time()
 # Calculate elapsed time
 elapsed_time = end_time - start_time
 print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
